a672c1429a4fd59ef4cf2a026444e73a49b5d051
Chandru mathan <chandru.mathan@gmail.com>
2023-06-06 09:01:17 +1000

    update app.py

--- app.py
+++ app.py
@@ -8,16 +8,10 @@ CORS(app)
 
 @app.route('/api/execute-script', methods=['POST'])
 def execute_script():
-    # Retrieve any data from the request if needed
-    # ...
-
-    # Execute your Python script here
-    # ...
     
     os.system('python3 /Users/ml/Desktop/ILS/integration/test_execution.py')
-    # Return a response if desired
     return 'Script executed successfully'
-    #return data_list
+    
 
 if __name__ == '__main__':
     app.run()
